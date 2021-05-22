16c80e830242902356bc89a5117d4bcabd2177bb
hungnguyenuet <hungnguyenuet@gmail.com>
2021-05-22 22:32:42 +0700

    First commit: generic type, generic method

---
+++ src/genericsdemo/Box.java
@@ -0,0 +1,20 @@
+package genericsdemo;
+
+public class Box<T, E> {
+
+    private T t;
+    private E e;
+
+    public T getT() {
+        return t;
+    }
+
+    public void setT(T t) {
+        this.t = t;
+    }
+
+    public E getE() {
+        return e;
+    }
+
+}
---
+++ src/genericsdemo/Pair.java
@@ -0,0 +1,8 @@
+package genericsdemo;
+
+public interface Pair <K, V>{
+
+    public K getKey();
+    public V getValue();
+
+}
