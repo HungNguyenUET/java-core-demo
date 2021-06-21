1bd1f6f2bbf48b91cf9e2f2a930bdc6ebf8d4d19
Hung Nguyen <hungnv4@eway.vn>
2021-06-21 10:46:10 +0700

    Executor

---
+++ src/variabledemo/Main.java
@@ -0,0 +1,13 @@
+package variabledemo;
+
+public class Main {
+
+    public static void main(String[] args) {
+        PrimitiveDataTypeDemo demo = new PrimitiveDataTypeDemo();
+        demo.showInstanceVar();
+
+        PrimitiveLiteralDemo demo1 = new PrimitiveLiteralDemo();
+        System.out.println(demo1.toString());
+    }
+
+}
