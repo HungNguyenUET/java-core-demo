0d46b62f4cc0db71725dfa1d7eb15705dc690f54
Hung Nguyen <hungnv4@eway.vn>
2021-10-04 21:52:05 +0700

    Concurrency: Understanding thread safety

--- src/main/concurrency/AwaitDemo.java
+++ src/main/concurrency/AwaitDemo.java
@@ -5,19 +5,46 @@ import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 
 public class AwaitDemo {
+    private static int count = 0;
+
     public static void main(String[] args) {
         ExecutorService service = null;
         try {
             service = Executors.newSingleThreadExecutor();
 
+            System.out.println("Start!");
+
+            service.submit(() -> {
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    System.out.println("Sleep thread get error!!!");
+                }
+
+                for (int i = 0; i < 500; i++) {
+                    count++;
+                }
+
+                System.out.println("Count: " + count + ", End!");
+            });
         } finally {
             if (service != null) {
+
+                System.out.println("Before await");
+
+                try {
+                    service.awaitTermination(3, TimeUnit.SECONDS);
+                } catch (InterruptedException e) {
+                    System.out.println("Thread running timeout");
+                }
+
+                System.out.println("After await");
                 service.shutdown();
             }
         }
 
-        if (service != null) {
-            service.awaitTermination(1, TimeUnit.MINUTES);
+        if (service.isShutdown()) {
+            System.out.println("Shutdown");
         }
 
         if (service.isTerminated()) {
@@ -25,5 +52,7 @@ public class AwaitDemo {
         } else {
             System.out.println("At least on task is still running");
         }
+
+        System.out.println("End of main function!");
     }
 }
---
+++ src/main/concurrency/SheepManager.java
@@ -0,0 +1,32 @@
+package main.concurrency;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class SheepManager {
+//    private AtomicInteger sheepCount = new AtomicInteger(0);
+    private int sheepCount = 0;
+    private final Object herd = new Object();
+
+    private void incrementAndReport() {
+//        System.out.println((sheepCount.incrementAndGet()) + " ");
+        synchronized (this) {
+            System.out.println((sheepCount++) + " ");
+        }
+    }
+
+    public static void main(String[] args) {
+        ExecutorService service = null;
+        try {
+            service = Executors.newFixedThreadPool(20);
+            SheepManager manager = new SheepManager();
+
+            for (int i = 0; i < 10; i++) {
+                service.submit(() -> manager.incrementAndReport());
+            }
+        } finally {
+            if (service != null) service.shutdown();
+        }
+    }
+}
